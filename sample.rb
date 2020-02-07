7efc686102f052e69cc3d6659f8213edc1678f73
kundanasakhamuri <60694277+kundanasakhamuri@users.noreply.github.com>
2020-02-07 13:18:15 +0530

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -6,7 +6,7 @@ class Students
   stud[4] = "4","name4","class4","fail",
   stud[1] = "1","name1","class1","pass"
   stud[2] = "2","name2","class2","fail"
-  stud.each{|id, rollno, name, grade , status| puts "key : #{id}, value : #{name} #{rollno} #{grade} #{status}"}
-end
+  stud.each{|id, rollno, name, grade , status| puts ""key : #{id}, value : #{name} #{rollno} #{grade} #{status}"}
+end,
 one=Students.new()
 num=gets
